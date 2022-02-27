657cf9ab987b79abac116b7b1862cefbedef992d
Davy de Lima Carvalho <davy.carvalho@ambevtech.com.br>
2022-02-27 11:26:53 -0300

    initial commit

---
+++ badCodeToRefactor/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace badCodeToRefactor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
